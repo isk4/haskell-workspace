fbddb62f242f401d1376d5fafbf09de4c1d184aa
isk4 <50377391+isk4@users.noreply.github.com>
2023-12-27 21:10:47 +0000

    Solve first 5 excercises from GitHub's course lecture 1

---
+++ GitHub Haskell Course/Lecture1.hs
@@ -0,0 +1,121 @@
+module Lecture1
+    ( makeSnippet
+    , sumOfSquares
+    , lastDigit
+    , minmax
+    , subString
+    , strSum
+    , lowerAndGreater
+    ) where
+
+-- VVV If you need to import libraries, do it after this line ... VVV
+
+-- ^^^ and before this line. Otherwise the test suite might fail  ^^^
+
+{- | Specify the type signature of the following function. Think about
+its behaviour, possible types for the function arguments and write the
+type signature explicitly.
+-}
+
+makeSnippet :: Int -> [Char] -> [Char]
+makeSnippet limit text = take limit ("Description: " ++ text) ++ "..."
+
+{- | Implement a function that takes two numbers and finds sum of
+their squares.
+
+>>> sumOfSquares 3 4
+25
+
+>>> sumOfSquares (-2) 7
+53
+
+Explanation: @sumOfSquares 3 4@ should be equal to @9 + 16@ and this
+is 25.
+-}
+-- DON'T FORGET TO SPECIFY THE TYPE IN HERE
+sumOfSquares :: Int -> Int -> Int
+sumOfSquares x y = x ^ 2 + y ^ 2
+
+{- | Implement a function that returns the last digit of a given number.
+
+>>> lastDigit 42
+2
+>>> lastDigit (-17)
+7
+
+🕯 HINT: use the @mod@ function
+
+-}
+-- DON'T FORGET TO SPECIFY THE TYPE IN HERE
+lastDigit :: Int -> Int
+lastDigit n = mod n 10
+
+{- | Write a function that takes three numbers and returns the
+difference between the biggest number and the smallest one.
+
+>>> minmax 7 1 4
+6
+
+Explanation: @minmax 7 1 4@ returns 6 because 7 is the biggest number
+and 1 is the smallest, and 7 - 1 = 6.
+
+Try to use local variables (either let-in or where) to implement this
+function.
+-}
+minmax :: Int -> Int -> Int -> Int
+minmax x y z = maximum (x : y : z : []) - minimum (x : y : z : [])
+    where
+        maximum :: [Int] -> Int
+        maximum (x:xs)
+            | null xs = x
+            | x > (head xs) = maximum (x : (tail xs))
+            | otherwise = maximum xs
+          
+        minimum :: [Int] -> Int
+        minimum (x:xs)
+            | null xs = x
+            | x < (head xs) = minimum (x : (tail xs))
+            | otherwise = minimum xs  
+
+{- | Implement a function that takes a string, start and end positions
+and returns a substring of a given string from the start position to
+the end (including).
+
+>>> subString 3 7 "Hello, world!"
+"lo, w"
+
+>>> subString 10 5 "Some very long String"
+""
+
+This function can accept negative start and end position. Negative
+start position can be considered as zero (e.g. substring from the
+first character) and negative end position should result in an empty
+string.
+-}
+subString :: Int -> Int -> [Char] -> [Char]
+subString start end str = take (end - start) (drop start str)
+
+{- | Write a function that takes a String — space separated numbers,
+and finds a sum of the numbers inside this string.
+
+>>> strSum "100    -42  15"
+73
+
+The string contains only spaces and/or numbers.
+-}
+strSum str = error "TODO"
+
+{- | Write a function that takes a number and a list of numbers and
+returns a string, saying how many elements of the list are strictly
+greater than the given number and strictly lower.
+
+>>> lowerAndGreater 3 [1 .. 9]
+"3 is greater than 2 elements and lower than 6 elements"
+
+Explanation: the list [1 .. 9] contains 9 elements: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+The given number 3 is greater than 2 elements (1 and 2)
+and lower than 6 elements (4, 5, 6, 7, 8 and 9).
+
+🕯 HINT: Use recursion to implement this function.
+-}
+lowerAndGreater n list = error "TODO"
